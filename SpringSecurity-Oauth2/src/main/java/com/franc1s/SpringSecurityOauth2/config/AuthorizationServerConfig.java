25e787acc8779c3c4d7fcb1724a56525ef37a7e2
franc1s <714954230@qq.com>
2021-11-02 15:31:46 +0800

    JSON Login

--- SpringSecurity-Oauth2/src/main/java/com/franc1s/SpringSecurityOauth2/config/AuthorizationServerConfig.java
+++
@@ -1,7 +0,0 @@
-package com.franc1s.SpringSecurityOauth2.config;
-
-
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-
-public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-}
